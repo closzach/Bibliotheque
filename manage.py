dc7fd4a60ad1516d2d71c438c53275a4b573330c
closzach <constantinloszach@gmail.com>
2025-08-28 11:59:33 +0200

    Renommage de la Médiathèque en Bibliothèque.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mediatheque.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bibliotheque.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
